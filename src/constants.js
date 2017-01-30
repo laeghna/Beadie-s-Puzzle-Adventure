4b5c414ccaf5b4f37cf4d19f909f62c5af80de1a
laeghna <laeghna@uw.edu>
2017-01-29 20:10:19 -0800

    Create and add constants.js to game

---
+++ src/constants.js
@@ -0,0 +1,23 @@
+//Character spritesheet frame width
+SHEET_W = 8;
+
+//Character sprite sheet index starting positions
+//where count starts at zero
+var N_START_IDLE = 24;
+var W_START_IDLE = 0;
+var E_START_IDLE = 4;
+
+var N_START_WALK = 32;
+var W_START_WALK = 8;
+var E_START_WALK = 16;
+
+//Frame count for character animations
+var IDLE_FRAME_COUNT = 4;
+var WALK_FRAME_COUNT = 8;
+
+//Character frame size
+CHAR_W = 42;
+CHAR_H = 42;
+
+//Character movement speed
+var SPEED = 1;
