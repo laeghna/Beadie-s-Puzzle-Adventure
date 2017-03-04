dd85597e7206d460fd3799ba2c24c43423e5c9f7
Lisa Taylor <laeghna@uw.edu>
2017-03-04 01:31:15 -0800

    Add encryptions and solutions for puzzles

--- src/violetWorldPuzzles.js
+++ src/violetWorldPuzzles.js
@@ -1,5 +1,144 @@
 var violetPuzzle1 = {
-    encryption: [],
-    solution: [],
-    maxWidth: 0
+    encryption: [
+        [V,K,G,U,Z,G],
+        [N,W,Q],
+        [Z,P,F]
+    ],
+    solution: [
+        [P,R,A,I,S,E],
+        [T,H,E],
+        [S,U,N]
+    ]
+};
+
+var violetPuzzle2 = {
+    encryption: [
+        [P,K,X],
+        [D,R,S,X],
+        [W,H],
+        [R],
+        [B,W,X]
+    ],
+    solution: [
+        [T,H,E],
+        [C,A,K,E],
+        [I,S],
+        [A],
+        [L,I,E]
+    ]
+};
+
+var violetPuzzle3 = {
+    encryption: [
+        [M,J,T,D],
+        [U,P,V,D],
+        [G,E,V,U],
+        [P,O,Q,T],
+        [O],
+        [D,J,G,X]
+    ],
+    solution: [
+        [D,O,E,S],
+        [T,H,I,S],
+        [U,N,I,T],
+        [H,A,V,E],
+        [A],
+        [S,O,U,L]
+    ]
+};
+
+var violetPuzzle4 = {
+    encryption: [
+        [P],
+        [I,P,V],
+        [K,N,F,F,B,L,B],
+        [P],
+        [B,H,P,E,L],
+        [F,M,L,A,B]
+    ],
+    solution: [
+        [A],
+        [M,A,N],
+        [C,H,O,O,S,E,S],
+        [A],
+        [S,L,A,V,E],
+        [O,B,E,Y,S]
+    ]
+};
+
+var violetPuzzle5 = {
+    encryption: [
+        [P,F,Q],
+        [N,V,X,W,A,T,K,B,Q],
+        [F,K],
+        [W,K],
+        [V,Z,K,X,A],
+        [F,V,M,A],
+        [F,J,P,Q]
+    ],
+    solution: [
+        [I,T,S],
+        [D,A,N,G,E,R,O,U,S],
+        [T,O],
+        [G,O],
+        [A,L,O,N,E],
+        [T,A,K,E],
+        [T,H,I,S]
+    ]
+};
+
+var violetPuzzle6 = {
+    encryption: [
+        [P,N,H,S,T,P,F],
+        [T,X],
+        [H,L,D,U],
+        [U,Q,U,L,V,H,S,T,P,F],
+        [T,X],
+        [R,U,L,O,T,H,H,U,Y]
+    ],
+    solution: [
+        [N,O,T,H,I,N,G],
+        [I,S],
+        [T,R,U,E],
+        [E,V,E,R,Y,T,H,I,N,G],
+        [I,S],
+        [P,E,R,M,I,T,T,E,D]
+    ]
+};
+
+var violetPuzzle7 = {
+    encryption: [
+        [N,G],
+        [A,R,S],
+        [G,D,E,S,R,U,B,P,K,L,S],
+        [V,L,W,W],
+        [B],
+        [D,N,P,J],
+        [G,N,X],
+        [A,R,S],
+        [G,Z,S,L],
+        [Q,B,K,E,J],
+        [H,L],
+        [N,W,B,O,L],
+        [Q,B,K,E,J],
+        [H,L],
+        [U,L,N,U]
+    ],
+    solution: [
+        [A,S],
+        [F,O,R],
+        [S,C,H,R,O,D,I,N,G,E,R],
+        [W,E,L,L],
+        [I],
+        [C,A,N,T],
+        [S,A,Y],
+        [F,O,R],
+        [S,U,R,E],
+        [M,I,G,H,T],
+        [B,E,],
+        [A,L,I,V,E],
+        [M,I,G,H,T],
+        [B,E,],
+        [D,E,A,D]
+    ]    
 };
