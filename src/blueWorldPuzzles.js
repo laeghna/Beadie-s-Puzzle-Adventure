992d3b3a34c1cc7c6507989b68118a87d960dbaf
Lisa Taylor <laeghna@uw.edu>
2017-03-04 05:19:18 -0800

    Add logic grid puzzle solutions

--- src/blueWorldPuzzles.js
+++ src/blueWorldPuzzles.js
@@ -1,5 +1,70 @@
 var bluePuzzle1 = {
-    solution: [],
-    width: 0,
-    height: 0
+    solution: [
+        [Al, FBI, Monday, 9],
+        [Gary, Explorer, Wednesday, 7],
+        [Mike, Family Tides, Sunday, 8],
+        [Roger, The Z-Files, Tuesday, 10]
+    ],
+    width: 12,
+    height: 12
+};
+
+var bluePuzzle2 = {
+    solution: [
+        [Brenda, Wish, May, 12, day],
+        [Cally, Ridge, July, 23, day],
+        [Linda, Scopefield, April, 4, month],
+        [Rachel, Mann, February, 17, day],
+        [Sheila, Enfield, September, 8, month]
+    ],
+    width: 20,
+    height: 20
+};
+
+var bluePuzzle3 = {
+    solution: [
+        [Gary, Willow, Solitude, Sammy, Brown],
+        [Richard, Lambert, Never Again, Sharon, White],
+        [Sara, Rondell, Angel, Ricky, Star],
+        [Tina, Stone, I Love You, Anette, Turner],
+        [Wanda, Belmont, Wild Ride, Mark, Right]
+    ],
+    width: 20,
+    height: 20
+};
+
+var bluePuzzle4 = {
+    solution: [
+        [Jeremy, 12, Grab It, 1, 4],
+        [Louis, 15, Mystifying, 4, 3],
+        [Michael, 14, Space Patrol, 2, 5],
+        [Nathaniel, 16, Battlefield, 3, 2],
+        [Steven, 13, Karate Master, 5, 1]
+    ],
+    width: 20,
+    height: 20
+};
+
+var bluePuzzle5 = {
+    solution: [
+        [Elliot, West, 1, Jupiter, Orion Nebula],
+        [Felix, Masters, 4, Neptune, Ring Nebula],
+        [Janet, Packet, 3, Mars, Crab Nebula],
+        [Nick, Spade, 2, Venus, Cat's Eye Nebula],
+        [Sheila, Carter, 12, Saturn, Omega Nebula]
+    ],
+    width: 20,
+    height: 20
+};
+
+var bluePuzzle6 = {
+    solution: [
+        [Elliot, Almond, manufacturing, Windows, Thursday],
+        [Goerge, Bixby, marketing, Apple, Monday],
+        [Harvey, Grace, engineering, Windows, Friday],
+        [John, Maxwell, customer service, Apple, Wednesday],
+        [Michael, Crawford, finance, Windows, Tuesday]
+    ],
+    width: 20,
+    height: 20
 };
